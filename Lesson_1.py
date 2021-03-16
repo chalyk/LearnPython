a59e0adc0e7a3fce63348ee1e9fbe10ffa91f96e
chalyk <45755801+chalyk@users.noreply.github.com>
2021-03-16 15:43:28 +0200

    Add files via upload

---
+++ Lesson_1.py
@@ -0,0 +1,30 @@
+print('Hello World!')  # Outputs Hello World!
+print('"Hello World!"')  # Outputs "Hello World!"
+print("'Hello World!'")  # Outputs 'Hello World!'
+print('Hello \' World!')  # Outputs Hello ' World!
+print('Hello \\ World!')  # Outputs Hello \ World!
+print('2+3')  # Outputs 2+3
+print(2+3)  # Outputs 5
+print('Hello, ' + 'Vlad')  # Outputs Hello, Vlad
+print('Hello', 'Vlad')  # Outputs Hello Vlad
+print('Hello', 'Vlad', 'and Liza')  # Outputs Hello Vlad and Liza
+print('Hello', 'Vlad', 'and Liza', sep='$$')  # Outputs Hello$$Vlad$$and Liza
+print('Hello', 'Vlad', 'and Liza', sep='')  # Outputs HelloVladand Liza
+
+print('Hello,\nVlad')  # Hello,
+                       # Vlad
+
+
+"""Homework
+ 1) Выведите с помощью двух функций print два предложения: 
+ «Я прохожу курс по Python» и «Я учу Python по курсу: ‘Программирование на Python с Нуля до Гуру’».
+ 2) Выведите с помощью только одного вызова функции print два предложения 
+ из предыдущего пункта, причём после первого предложения должен быть переход на новую строку.
+"""
+
+print('Я прохожу курс по Python')
+print('Я учу Python по курсу: ‘Программирование на Python с Нуля до Гуру’')
+
+print('Я прохожу курс по Python', '\nЯ учу Python по курсу: ‘Программирование на Python с Нуля до Гуру’')
+
+#Well Done
